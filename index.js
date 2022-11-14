05e219a2e09cfe6c81d53a1a965b24abc5ec5935
wendy <mizukilee@gmail.com>
2022-11-13 20:16:46 -0500

    commit tree 31f8211aba9abb32b74cb7091244ef646ae78cea

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const { defaultDocumentFeatures } = require("jsdom");
+
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  
+  function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left);
+    dodger.style.left = `${left + 1}px`;
+  } 
+
+  document.addEventListener("keydown", function(e){
+    const key = e.key
+    if (key === "ArrowRight"){
+        //console.log(e);
+        moveDodgerRight();
+    }
+});
+
+  
